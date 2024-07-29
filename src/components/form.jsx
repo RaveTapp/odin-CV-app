41eb8495aab341e91ec2b7984334b31d2f08673f
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-07-29 10:42:59 +0200

    Handle submit

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 
 import { Input, School, Job } from "./input.jsx";
 
+
+
 export function Form(){
     const [numSchools, setNumSchools] = useState(1);
     const [numJobs, setNumJobs] = useState(0);
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
     const schools = [];
     for(let i = 0; i < numSchools; i++){
@@ -15,23 +18,33 @@ export function Form(){
     for(let i = 0; i < numJobs; i++){
         jobs.push(<Job key={i} n={i} />);
     }
+    if(!isSubmitted){
+        return <form onSubmit={(event) => {handleSubmit(event, setIsSubmitted)}}>
+            <section className="general">
+                <h1>General information</h1>
+                <Input label="Fullname: " id="name" />
+                <Input label="Email: " id="email" />
+                <Input label="Phone number: " id="phone" />
+            </section>
+            <section className="education">
+                <h1>Education</h1>
+                {schools}
+                <button onClick={() => setNumSchools(numSchools + 1)}>+</button>
+            </section>
+            <section className="experience">
+                <h1>Job experience</h1>
+                {jobs}
+                <button onClick={() => setNumJobs(numJobs + 1)}>+</button>
+            </section>
+            <button type='submit'>Submit</button>
+        </form>
+    }
+    return <>
+        <h1>Test CV</h1>
+    </>
+}
 
-    return <form>
-        <section className="general">
-            <h1>General information</h1>
-            <Input label="Fullname: " id="name" />
-            <Input label="Email: " id="email" />
-            <Input label="Phone number: " id="phone" />
-        </section>
-        <section className="education">
-            <h1>Education</h1>
-            {schools}
-            <button onClick={() => setNumSchools(numSchools + 1)}>+</button>
-        </section>
-        <section className="experience">
-            <h1>Job experience</h1>
-            {jobs}
-            <button onClick={() => setNumJobs(numJobs + 1)}>+</button>
-        </section>
-    </form>
+function handleSubmit(event, handleState){
+    event.preventDefault();
+    handleState(true);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {Form} from "./components/form.jsx"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(  
   <React.StrictMode>
-    <Form></Form>
+      <Form />
   </React.StrictMode>
 );
