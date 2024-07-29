7b92d12d777f00ab5c8cdfd04116d8cc0e1c8b5f
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-07-29 07:12:15 +0200

    Add input for school data

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react';
+
 import { Input } from "./input.jsx";
+import { School } from "./school.jsx"
+
 
 export function Form(){
+    const [numSchools, setNumSchools] = useState(1);
+
+    const schools = [];
+    for(let i = 0; i < numSchools; i++){
+        schools.push(<School key={i} n={i} />);
+    }
+    
     return <form>
         <section className="general">
             <Input label="Fullname: " id="name" />
@@ -8,7 +19,7 @@ export function Form(){
             <Input label="Phone number: " id="phone" />
         </section>
         <section className="education">
-            
+            {schools}
         </section>
         <section className="experience">
 
--- src/components/school.jsx
+++ src/components/school.jsx
@@ -1,3 +1,19 @@
-export function School(){
-    
+import { Input } from "./input.jsx";
+
+export function School({n}){
+    return <div>
+        <Input label="School name: " id={"school" + n} ></Input>
+        <Input label="Title of study: " id={"study" + n} ></Input>
+        <FromToInput n={n} />
+    </div>
+}
+
+function FromToInput({n}){
+    return <>
+        <label htmlFor={"from" + n} > From </label>
+        <input id={"from" + n} type="number" min={1900} max={2100} required ></input>
+        <label htmlFor={"to" + n} > to </label>
+        <input id={"to" + n} type="number" min={1900} max={2100} required ></input>
+
+    </>
 }
